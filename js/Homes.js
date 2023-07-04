649f898b8f436d1ba16b947524e1c4dc864b539e
jaber ibrahim <jaber.ibrahim0937@gmail.com>
2023-07-04 17:09:10 +0300

    aaa

--- js/Homes.js
+++ js/Homes.js
@@ -8,9 +8,9 @@ lis.forEach(li => {
     })
 });
 
-
+let myUrl = ("./../JsonFiles/Homes.json");
 async function fetchcards() {
-    await fetch("./../JsonFiles/Homes.json")
+    await fetch(myUrl)
     .then((result) => result.json())
     .then((result) => {
       data = result.homes;
